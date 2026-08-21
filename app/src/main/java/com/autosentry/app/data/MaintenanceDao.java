cf973383cefbb3d423d6f0481a1553cb5ac05fd4
EL-Hunter <ellishuntersmith@gmail.com>
2026-08-20 22:53:34 -0500

    Add MaintenanceDao

---
+++ app/src/main/java/com/autosentry/app/data/MaintenanceDao.java
@@ -0,0 +1,24 @@
+package com.autosentry.app.data;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import androidx.room.Delete;
+
+import java.util.List;
+
+@Dao
+public interface MaintenanceDao {
+    @Insert
+    long insert(MaintenanceEvent evt);
+
+    @Update
+    void update(MaintenanceEvent evt);
+
+    @Delete
+    void delete(MaintenanceEvent evt);
+
+    @Query("SELECT * FROM maintenance_events ORDER BY nextServiceAt ASC")
+    List<MaintenanceEvent> dueSoon();
+}
